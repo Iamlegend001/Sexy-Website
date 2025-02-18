c7ce296f013c18920a3e6402e3802316e6ea7c4b
Iamlegend001 <147546241+Iamlegend001@users.noreply.github.com>
2025-02-18 20:06:35 +0530

    2nd and 3rd section

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
-gsap.to("#nav",{
-  backgroundColor:"#000",
-  duration:0.5,
-  height:"5rem",
-  scrollTrigger:{
-    trigger:"#nav",
-    scroller:"body",
-    // markers:true,
-    start:"top -10%",
-    end:"top -11%",
-    scrub:1,
-  }
-})
+var cursor = document.querySelector("#cursor");
+var blr = document.querySelector("#cursor-blr");
+document.addEventListener("mousemove", (dets) => {
+  cursor.style.left = dets.x + "px";
+  cursor.style.top = dets.y + "px";
+  blr.style.left = dets.x-200 + "px";
+  blr.style.top = dets.y-200 + "px";
+});
 
-gsap.to("#main",{
-  backgroundColor:"#000",
-  scrollTrigger:{
-    trigger:"#main",
-    scroller:"body",
-    start:"top -50%",
-    end:"top -100vh",
-    scrub:3,
-  }
-})
+
+
+gsap.to("#nav", {
+  backgroundColor: "#000",
+  duration: 0.5,
+  height: "5rem",
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    // markers:true,
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 1,
+  },
+});
+
+gsap.to("#main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    start: "top -50%",
+    end: "top -100vh",
+    scrub: 3,
+  },
+});
